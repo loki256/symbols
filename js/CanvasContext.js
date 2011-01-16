b3b39c42d245975d7214dda3f0f6b493415eaea0
loki <loki.m.256@gmail.com>
2011-01-16 21:57:19 +0300

    Add load map from file. Closes gh-1
    Add css for inputs
    Add nginx config for project

--- js/CanvasContext.js
+++ js/CanvasContext.js
@@ -7,15 +7,31 @@
 // CanvasContext class
 // -------------------------
 Symbols.CanvasContext = Class ({
+
     initialize: function(ctx) {
         this.ctx = ctx;
     },
+
     save: function() {
         this.ctx.save();
     },
-    strokeStyle: function(style) {
+
+    setStrokeStyle: function(style) {
+        if (this.saved_stroke_style == style) {
+            return;
+        }
         this.ctx.strokeStyle = style;
+        this.saved_stroke_style = style;
     },
+
+    setFillStyle: function(style) {
+        if (this.saved_fill_style == style) {
+            return;
+        }
+        this.ctx.fillStyle = style;
+        this.saved_fill_style = style;
+    },
+
     setFont: function(new_font) {
         if (this.saved_font == new_font) {
             return;
